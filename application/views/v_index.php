dc315677d0af6bf679931fb61e33e9e3de25f538
Dedenzhulfa <dedenzm01@gmail.com>
2023-10-10 22:13:32 +0700

    Pertemuan 3 -membuat view header,index dan footer

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -4,7 +4,7 @@
 tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu
 MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih 
 jauh dalam belajar codeigniter.</p>
-            <p>MVC adalah teknik atau konsep yang memisahkan 
+            <p>MVC adalah sebuah teknik atau konsep yang memisahkan 
 komponen utama menjadi tiga komponen yaitu model, view dan 
 controller.</p>
 
