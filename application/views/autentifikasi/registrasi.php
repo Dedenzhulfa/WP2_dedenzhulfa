4923bf346bd0dbafb3ffaf9cb4d3bfa8b67e969d
Dedenzhulfa <dedenzm01@gmail.com>
2023-11-17 23:41:01 +0700

    Pertemuan 9 menambahkan tampilan registasi

--- application/views/autentifikasi/registrasi.php
+++ application/views/autentifikasi/registrasi.php
@@ -8,8 +8,7 @@
  <div class="text-center">
  <h1 class="h4 text-gray-900 mb-4">Daftar Menjadi Member!</h1>
  </div>
- <form class="user" method="post"
-action="<?= base_url('autentifikasi/registrasi'); ?>">
+ <form class="user" method="post" action="<?= base_url('autentifikasi/registrasi'); ?>">
  <div class="form-group">
  <input type="text" class="form-control form-control-user" id="nama" name="nama" placeholder="Nama 
 Lengkap" value="<?= set_value('nama'); ?>">
@@ -38,8 +37,7 @@ name="password2" placeholder="Ulangi Password">
 '<small class="text-danger pl-3">', '</small>'); ?>
  </div>
  </div>
- <button type="submit" class="btn btn-primary btn-user btn-block">
- Daftar Menjadi Member
+ <button type="submit" class="btn btn-primary btn-user btn-block">Daftar Menjadi Member
  </button>
  </form>
  <hr>
