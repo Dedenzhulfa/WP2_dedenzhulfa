a6147109ae4e7525a340bed452df805fbe469ddb
Dedenzhulfa <dedenzm01@gmail.com>
2023-11-17 23:02:24 +0700

    Pertemuan 7 menambahkan view admin sidebar.php (revisi)

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -1,4 +1,4 @@
-<!-- Bagian Sidebar -->
+<!-- ini adalah Bagian Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
  <!-- Sidebar - Brand -->
  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html"
